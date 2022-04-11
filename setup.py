40e2fc48c1e618a5c23f96e163f76ff01b6a65a9
Kalyan Kumar Mohanty <55730742+KalyanMohanty@users.noreply.github.com>
2022-04-11 21:51:00 +0530

    'v6'

--- setup.py
+++ setup.py
@@ -11,13 +11,15 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="fvisionNetwork14",
-    version="0.0.4",
-    description="fvisionNetwork14 is classification model, image preprocessing module will preprocess the image, plotting accuracy and loss grapghs plots the graphs",
+    version="0.0.6",
+    description="fvisionNetwork14 is an image classification CNN model that can classify the  number of classes.",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/KalyanMohanty/fvisionNetwork14",
     author="Kalyan Mohanty",
     author_email="kalyanrisingstar@gmail.com",
+    keywords = ['fvisionNetwork14', 'fvNet14', 'image classification', 'deep neural network',
+    'cnn', 'keras model', 'cnn'],
     license="MIT",
     classifiers=[
         'Intended Audience :: Science/Research',
@@ -27,13 +29,13 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.8",
     ],
+    
     # packages=["fvisionNetwork14"],
     include_package_data=True,
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     install_requires=[
                     "matplotlib",
-                    "numpy"
-                    
+                    "numpy"           
     ]
 )
